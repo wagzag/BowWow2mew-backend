e8961f5ac6c7bc0fc99294faf50a19f744e10b24
Eunyeong Jang <jangeunyeong081@gmail.com>
2024-06-17 20:22:18 +0900

    Fix :eslint 적용안됨 -> 오타수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,13 @@
-module.export = [
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    rules: {
-      'no-var': 'error',
-      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
-      'no-multiple-empty-lines': 'error',
-      eqeqeq: 'error',
-      'no-unused-vars': 'error',
-    },
+module.exports = {
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-];
+  rules: {
+    'no-var': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+    'no-multiple-empty-lines': 'error',
+    eqeqeq: 'error',
+    'no-unused-vars': 'error',
+  },
+};
