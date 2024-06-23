77c3f7e6d667f41ac661a5b13faebb086eab27a7
Eunyeong Jang <jangeunyeong081@gmail.com>
2024-06-23 13:02:21 +0900

    Feat : commnent, post 스키마 정의 및 모델 생성

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User'); // 상대 경로 수정
+const User = require('../models/schemas/User'); // 상대 경로 수정
 
 const router = express.Router();
 
