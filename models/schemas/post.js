443468c7b171c99f7f56f48e22b0c7f71cf5f510
yoon-gang00 <yk1008@konkuk.ac.kr>
2024-06-26 23:32:50 +0900

    머지로 인한 에러 해결(aysnc부분 삭제)

--- models/schemas/post.js
+++ models/schemas/post.js
@@ -1,42 +1,39 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const getShortId = require('./types/short-id'); // shortId 함수
+const shortId = require('./types/short-id'); // shortId 함수
 const moment = require('moment'); // 날짜 형식화를 위해 moment.js 사용
 
-async function createPostSchema() {
-  const shortId = await getShortId;
-
-  const PostSchema = new Schema(
-    {
-      postId: shortId,
-      title: {
-        type: String,
-        required: true,
-      },
-      content: {
-        type: String,
-        required: true,
-      },
-      userName: {
-        type: String,
-        required: true,
-      },
-      createdAt: {
-        type: String,
-        default: () => moment().format('YYYY-MM-DD HH:mm'),
-      },
-      category: {
-        type: String,
-        required: true,
-        enum: ['freeboard', 'adoption', 'questions', 'boasts', 'recommend'],
-      },
+const PostSchema = new Schema(
+  {
+    postId: {
+      type: String,
+      default: shortId,
     },
-    {
-      timestamps: true,
+    title: {
+      type: String,
+      required: true,
     },
-  );
+    content: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: String,
+      default: () => moment().format('YYYY-MM-DD HH:mm'),
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ['freeboard', 'adoption', 'questions', 'boasts', 'recommend'],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-  return mongoose.models.Post || mongoose.model('Post', PostSchema);
-}
-
-module.exports = createPostSchema;
+module.exports = PostSchema;
